8c2dc4a0a51199ac7af3c119fed163d0039738f9
asoppitt <asoppitt-42@hotmail.co.uk>
2023-06-16 14:21:35 +0100

    nonoe

--- src/save_as_you_go.jl
+++ src/save_as_you_go.jl
@@ -148,7 +148,7 @@ end
 function omega_step!(omegap::Omega{T,LogNormal},dt::T) where T<:AbstractFloat
     #E-M solver for LogNormal
     dw = sqrt(dt).*randn(T,size(omegap,1)) #random draws
-    omegap.log_omega[:] .+=  .-(omegap.inv_T_omega).*(omegap.log_omega.+omegap.log_sigma_2-log.(omegap.omega_bar)).*dt.+sqrt.(2*omegap.inv_T_omega*omegap.log_sigma_2*omegap.omega_bar).*dw
+    omegap.log_omega[:] .+=  .-(omegap.inv_T_omega).*(omegap.log_omega.+omegap.log_sigma_2.-log.(omegap.omega_bar)).*dt.+sqrt.(2*omegap.inv_T_omega*omegap.log_sigma_2*omegap.omega_bar).*dw
 
     omegap[:] = exp.(omegap.log_omega)
 
