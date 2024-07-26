d6af5a2bfca4a70dcce938f83df416a2f3db6938
asoppitt <asoppitt-42@hotmail.co.uk>
2024-07-26 15:29:11 +0100

    now all relevent files are actually included

--- src/TurbulentMixingParticleTrackingReactions.jl
+++ src/TurbulentMixingParticleTrackingReactions.jl
@@ -1,5 +1,6 @@
 module TurbulentMixingParticleTrackingReactions
     include("PSP_Particletracking_module.jl")
+    include("stepwise_funcs.jl")
     include("save_as_you_go.jl")
     include("return_as_array.jl")
 end
